595a1a44a5a025e1704d799d44094f671f0081d1
absolutedev <motpetrov080712@gmail.com>
2026-03-22 12:11:56 +0300

    Create Items.lua

---
+++ ItemShop/Shared Modules/Items.lua
@@ -0,0 +1,11 @@
+-- // Module that contains item stats with price & image
+-- // Path: ReplicatedStorage.Modules.Items 
+
+local Items = {
+	["Burger"] = {
+		Price = 100,
+		Image = "rbxassetid://43089258"
+	}
+}
+
+return Items 
