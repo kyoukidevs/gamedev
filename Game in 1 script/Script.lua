a3d4ea4a6e4662c0b5cf78b37ac2b20330f18479
absolutedev <motpetrov080712@gmail.com>
2026-03-25 04:09:38 +0300

    Update Script.lua

--- Game in 1 script/Script.lua
+++ Game in 1 script/Script.lua
@@ -1,4 +1,5 @@
 -- // Simple minigame, run from tsunami to win 
+-- // Updated: i forgot comments
 
 --#region Variables
 
@@ -44,16 +45,18 @@ LoadedPlayers.__index = LoadedPlayers -- // For MT
 
 --#region Players Connections
 
-Players.PlayerAdded:Connect(function(Client)
+Players.PlayerAdded:Connect(function(Client) -- // Init Client After Connecting
 	task.wait(1.5)
-	
+
+	-- // Create leaderstats & wins
 	local Leaderstats = Instance.new("Folder", Client)
 	Leaderstats.Name = "leaderstats"
 	
 	local Wins = Instance.new("IntValue", Leaderstats)
 	Wins.Name = "Wins"
 	Wins.Value = 0 
-	
+
+	-- // setmetatable for custom methods
 	LoadedPlayers[Client] = setmetatable({
 		Instance = Client,
 		Character = Client.Character,
@@ -61,6 +64,7 @@ Players.PlayerAdded:Connect(function(Client)
 	}, LoadedPlayers)
 end)
 
+-- // Remove client data when he is removing
 Players.PlayerRemoving:Connect(function(Client)
 	if LoadedPlayers[Client] then
 		LoadedPlayers[Client]:Remove(true)
@@ -71,45 +75,47 @@ end)
 --#region LoadedPlayers Methods
 
 LoadedPlayers.Spawn = function(self)
+	-- // Checks for character ~= nil and theres map in workspace
 	if self.Character and Globals.Map then 
-		local SpawnPoint = Globals.Map:FindFirstChild("SpawnPart")
+		local SpawnPoint = Globals.Map:FindFirstChild("SpawnPart") -- // Find spawnpoint and pivot character to its cframe
 		if SpawnPoint then 
 			self.Character:PivotTo(SpawnPoint.CFrame)
 		end
 		
-		PlayingPlayers[self.Instance] = true 
+		PlayingPlayers[self.Instance] = true -- // Insert player to playingplayers
 		
-		self.Playing = true   
+		self.Playing = true  -- // Set playerdata.playing boolean to true
 		
 		self.Humanoid.Died:Connect(function()
 			self:Remove(false)
-		end)
+		end) -- // Remove from game after player died
 	end
 end
 
 LoadedPlayers.Remove = function(self, DestroyData)
-	if self.Playing and PlayingPlayers[self.Instance] then
+	if self.Playing and PlayingPlayers[self.Instance] then -- // Checks if player is playing
 		PlayingPlayers[self.Instance] = nil 
-		if self.Humanoid then 
+		if self.Humanoid then -- // Checks for humanoid ~= nil and damaging him by 9e9
 			self.Humanoid:TakeDamage(9e9)
 		end
 	end
 	
-	if DestroyData then 
-		LoadedPlayers[self.Instance] = nil 
+	if DestroyData then
+		LoadedPlayers[self.Instance] = nil -- // Destroying data if destroydata boolean is true
 	end
 end
 --#endregion 
 
 --#region Rounds Logic
 
-local function CreateWave()
+local function CreateWave() -- // create wave func
 	if not (Globals.GameStarted and Globals.Map) then
-		return 
+		return -- //return if game is not started and theres no map
 	end
 	
-	Globals.LastWave = tick()
-	
+	Globals.LastWave = tick() -- // Update lastwave tick
+
+	-- // Create wave part
 	local GoalPart = Globals.Map.WinPart
 	local StartPart = Globals.Map.WaveStart
 	local Wave = Instance.new("Part", Globals.Map)
@@ -121,7 +127,8 @@ local function CreateWave()
 	
 	 
 	--Wave.CFrame = Wave.CFrame * CFrame.Angles(0, Angle, 0)
-	
+
+	-- // select random wave
 	local random = math.random(1, 100)
 	local PassedWaves = {}
 	local SelectedWave 
@@ -133,7 +140,7 @@ local function CreateWave()
 	end
 	
 	if #PassedWaves > 0 then 
-		SelectedWave = PassedWaves[math.random(1, #PassedWaves)]
+		SelectedWave = PassedWaves[math.random(1, #PassedWaves)] -- // random index from passedwaves table
 	else
 		SelectedWave = "Classic"
 	end
@@ -151,61 +158,69 @@ local function CreateWave()
 	})
 	
 	Tween:Play()
-	
+
+	-- // fires signal when wave part getting touched
 	Wave.Touched:Connect(function(HitInstance)
 		if HitInstance and HitInstance.Parent:FindFirstChildOfClass("Humanoid") then
+			-- // if theres a hitinstance and hitinstance.parent (Character) have a humanoid 
 			local Player = LoadedPlayers[Players:GetPlayerFromCharacter(HitInstance.Parent)]
 			
-			if Player and Player.Playing and HitInstance.Position.Y > 8 then
-				Player:Remove(false)
+			if Player and Player.Playing and HitInstance.Position.Y > 8 then -- // for tall avatars check Y position
+				Player:Remove(false) -- // remove player from game without destroying data
 			end
 		end
 	end)
 	Tween.Completed:Connect(function()
-		Wave:Destroy()
+		Wave:Destroy() -- // destroy wave after tween completed
 	end)
 end
 
 local function StartRound()
+	-- // update glboals
 	Globals.Map = Map:Clone()
 	Globals.Map.Parent = Workspace 
 	
 	for Index, Value in LoadedPlayers do
-		if type(Value) ~= "function" then 
-			Value:Spawn()
+		if type(Value) ~= "function" then -- // theres a functions in loadedplayers
+			Value:Spawn() -- // spawn player 
 		end
 	end
-	
-	Globals.RoundStart = tick()
+
+	-- // update globals
+	Globals.RoundStart = tick() 
 	Globals.GameStarted = true 
 	
 	local WinPart = Globals.Map.WinPart 
 	local Debounce = false 
-	
+
+	-- // debounce to not get multiple wins at once
 	WinPart.Touched:Connect(function(HitInstance)
-		if not Debounce and HitInstance and HitInstance.Parent:FindFirstChildOfClass("Humanoid") then
+		if not Debounce and HitInstance and HitInstance.Parent:FindFirstChildOfClass("Humanoid") then -- // check for humanoid
 			local Player = LoadedPlayers[Players:GetPlayerFromCharacter(HitInstance.Parent)]
 			
 			if Player and Player.Wins then
 				Debounce = true 
 				Player.Wins.Value += 1
 				Player:Remove()
+				Debounce = false 
 			end
 		end
 	end)
 end
 
 local function EndRound()
+	-- // update globals
 	Globals.LastWave = nil 
 	Globals.RoundEnd = tick()
 	Globals.GameStarted = false 
-	
+
+	-- // destroy map
 	if Globals.Map then
-		Globals.Map:Destroy()
+		Globals.Map:Destroy() -- // Globals.Map will set to nil automatically
 	end
 	
 	for Index, Value in PlayingPlayers do
-		Value:Remove(false)
+		Value:Remove(false) -- // Remove player from game without destryoing data
 	end
 end
 --#endregion
@@ -213,23 +228,21 @@ end
 --#region Loop
 
 RunService.Heartbeat:Connect(function()
-	for Index, Value in LoadedPlayers do
+	for Index, Value in LoadedPlayers do -- // players loop
 		if type(Value) ~= "table" then 
 			continue 
 		end
-		
+
+		-- // update data
 		Value.Character = Index.Character
 		Value.Root = Value.Character and Value.Character:FindFirstChild("HumanoidRootPart")
 		Value.Humanoid = Value.Character and Value.Character:FindFirstChildOfClass("Humanoid")
 	end
-	
+
+	-- // update indicator text
 	local Indicator = Workspace:FindFirstChild("Indicator")
 	if Indicator then
 		local Text = Indicator.SurfaceGui.TextLabel
-		
-		if Globals.GameBlocked and Globals.BlockedReason then
-			Text.Text = Globals.BlockedReason 
-		end
 		
 		if not Globals.GameStarted then
 			local Elapsed = tick() - Globals.RoundEnd
@@ -241,7 +254,8 @@ RunService.Heartbeat:Connect(function()
 		end
 	end
 
-	if not Globals.GameBlocked and not Globals.GameStarted and Globals.RoundEnd and (tick() - Globals.RoundEnd >= Globals.LobbyTime) then
+	-- // start round if game is not started and theres a round end tick and tick - globals.roundede > time in lobby
+	if not Globals.GameStarted and Globals.RoundEnd and (tick() - Globals.RoundEnd >= Globals.LobbyTime) then
 		StartRound()
 	end
 	
@@ -250,13 +264,15 @@ RunService.Heartbeat:Connect(function()
 	for Index, Value in PlayingPlayers do
 		TotalPlaying += 1 
 	end
-	
+
+	-- // end round if game is started and totalplaying is 0 or theres a round start tick and tick - round start tick > round time
 	if (Globals.GameStarted and TotalPlaying == 0) or Globals.RoundStart and tick() - Globals.RoundStart > Globals.RoundTime then
 		EndRound()
 	end
-	
+
+	-- // create waves
 	if Globals.GameStarted then
-		if not Globals.LastWave and tick() - Globals.RoundStart > 10 then
+		if not Globals.LastWave and tick() - Globals.RoundStart > 10 then -- // even if lastwave = nil wait 10 seconds
 			CreateWave()
 		end
 		
